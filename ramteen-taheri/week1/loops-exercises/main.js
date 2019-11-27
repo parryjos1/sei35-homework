f1ed98e591353c9cf4ee6952951c8ef7b276e855
Ramteen Taheri <ramteen.taheri@gmail.com>
2019-11-27 15:42:39 +1100

    finished loop exercises

--- ramteen-taheri/week1/loops-exercises/main.js
+++ ramteen-taheri/week1/loops-exercises/main.js
@@ -14,7 +14,7 @@ for(let x = 0; x <= 20; x++) {
 //multiplication tables
 for(let x = 0; x <= 10; x++) {
     for(let y = 0; y <= 10; y++) {
-        console.log(`${x} * ${y} = ` + x*y);
+        console.log(`${x} * ${y} = ${x*y}`);
     }  
     console.log("******************");
 }
