1df9a99f0c2e6fc6dabb7e6d7b63272618f04dda
Ramteen Taheri <ramteen.taheri@gmail.com>
2019-11-29 10:03:33 +1100

    leap year exercise

--- ramteen-taheri/week1/leap-year/js/main.js
+++ ramteen-taheri/week1/leap-year/js/main.js
@@ -11,10 +11,23 @@ function isLeapYear(year) {
 }
 
 let yearInput = prompt("Enter the year: ");
-yearInput = +yearInput;
-let flag = isLeapYear(yearInput);
-if(flag == true) {
-    console.log(`${yearInput} is a leap year!`);
-} else {
-    console.log(`${yearInput} is not a leap year :(`);
+if (yearInput !== null) {
+    while(yearInput.length > 4 || isNaN(yearInput)) {
+        console.log("Invalid Year Entered...");
+        yearInput = prompt("Enter the year: ");
+        if(yearInput == null) {
+            break;
+        }
+    }
+    if(yearInput) { 
+        yearInput = +yearInput;
+        let flag = isLeapYear(yearInput);
+        if(flag == true) {
+            console.log(`${yearInput} is a leap year!`);
+        } else {
+            console.log(`${yearInput} is not a leap year :(`);
+        }
+    }
 }
+console.log("You have quit the appliation.");
+
