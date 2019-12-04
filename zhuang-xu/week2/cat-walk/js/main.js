dc4fdb13ae2fab1e5c94c95bea266efadda315c8
Zhuang Xu <not-real@example.com>
2019-12-05 09:19:47 +1100

    Added more comments to the cat-walk homework

--- zhuang-xu/week2/cat-walk/js/main.js
+++ zhuang-xu/week2/cat-walk/js/main.js
@@ -8,6 +8,8 @@ let handle;
 
 const walk = function() {
   let newLeftPos = parseInt(cat.style.left) + direction * 10;
+
+  // Check if cat hits either right or left wall
   if (newLeftPos + cat.width > window.innerWidth || newLeftPos < 0) {
     direction = -1 * direction;
     if (direction === -1) {
@@ -17,13 +19,19 @@ const walk = function() {
     }
   }
 
+  // Cat steps into the middle of the stage
   if (Math.abs(
       newLeftPos + 0.5 * cat.width - window.innerWidth /
       2) < 10) {
     newLeftPos += direction * 10;
+
+    // Stops walking by clearing the interval callback
     clearInterval(handle);
+
     cat.src = 'images/dancing-cat.gif';
     document.body.className = 'dancing';
+
+    // Resume walking after dancing for a while
     setTimeout(() => {
       cat.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
       document.body.removeAttribute('class');
