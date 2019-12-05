908af5681aab43389b1d1fc85a8027b4cc2a429e
Gabriel Hilti <gabriel.hilti@gmail.com>
2019-12-05 22:38:11 +1100

    wk2 day4 catWalk logic

--- gabriel-hilti/week2/catWalk/js/main.js
+++ gabriel-hilti/week2/catWalk/js/main.js
@@ -1,107 +1,109 @@
 const img = document.querySelector('img');
 
-let pos = 0;
-img.style.left = pos;
-const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-const  viewport = w - 300;
-let direction = 'right';
-let leftPosId = null;
-let col1 = 'white', col2 = 'white', col3 = 'white';
-let rot = 0;
+const w = window.innerWidth,
+      h = window.innerHeight,
+      viewport = w - 300;
+let pos = 0,
+    direction = true,
+    intervalId = null;
 
 const randomCol = function() {
-  const r = Math.floor(Math.random()*256);
-  const g = Math.floor(Math.random()*256);
-  const b = Math.floor(Math.random()*256);
-  const col = [r, g, b];
-  return col;
+  const maxHex = 16**6;
+  const col = Math.floor(Math.random()*maxHex);
+  return col.toString(16);
 };
-
-const colorPicker = setInterval(function() {
-  col1 = randomCol();
-  col2 = randomCol();
-  col3 = randomCol();
-  console.log(col1);
-}, 100);
 
 const crazy = function() {
   document.body.style.width = '100vw';
   document.body.style.height = '100vh';
   img.src = "https://media.giphy.com/media/vIQm4HIpqnIt2/giphy.gif";
+  let col1, col2, col3, rot = 0;
+
+  const colorPicker = setInterval(function() {
+    col1 = randomCol();
+    col2 = randomCol();
+    col3 = randomCol();
+  }, 50);
+
   document.body.addEventListener('mousemove', function(e) {
-    rot += 1;
-    document.body.style.background = `radial-gradient(closest-side at ${(e.clientX/w)*100}% ${(e.clientY/h)*100}%, rgb(${col1[0]},${col1[1]}, ${col1[2]}), rgb(${col2[0]},${col2[1]}, ${col2[2]}), rgb(${col3[0]},${col3[1]}, ${col3[2]})`;
+    rot += 15;
+    document.body.style.background = `radial-gradient(closest-side at ${(e.clientX/w)*100}% ${(e.clientY/h)*100}%, #${col1}, #${col2}, #${col3}`;
     img.style.top = `${e.clientY}px`;
     img.style.left = `${e.clientX}px`;
     img.style.transform = `rotate(${rot}deg)`;
   });
 
-
 };
+
+const walk = function() {
+
+  if (direction) {
+    pos += 10;
+  } else {
+    pos -= 10;
+  }
+
+  img.style.left = pos + 'px';
+  console.log(img.style.left);
+};
+
+const walkTask1 = function() {
+  if (pos < viewport) {
+    walk();
+  } else {
+    pos = 0;
+  }
+};
+
+const walkTask2 = function() {
+  if (pos >= viewport - 10) {
+    img.style.transform = 'scaleX(-1)';
+    direction = !direction;
+  } else if (!direction && pos < 10 ) {
+    img.style.transform = 'scaleX(1)';
+    direction = !direction;
+  }
+  walk();
+};
+
+const walkTask3 = function() {
+  if (Math.abs(pos - (viewport/2)) <= 10 ) {
+    walk(); // hack: walks two times in a row when emerged in the middle otherwise condition could occur twice
+    clearInterval(intervalId);
+    img.src = 'images/giphy.gif';
+    setTimeout(function() {
+      img.src= 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+      intervalId = setInterval(function() {
+        walk();
+        catWalk(3);
+      }, 50);
+    }, 2000);
+  } else {
+    walkTask2();
+  }
+};
+
 
 const catWalk = function(option) {
-  if (option === 1) {
-    if (pos < viewport) {
-      pos += 10;
-    } else {
-      pos = 0;
-    }
-  } else if (option === 2) {
-      if (direction === 'right' && pos < viewport) {
-        pos += 10;
-      } else if (direction === 'right' && pos >= viewport) {
-        direction = 'left';
-        img.style.transform = 'scaleX(-1)';
-      } else if (direction === 'left' && pos >= 10) {
-        pos -= 10;
-      } else if (direction === 'left' && pos < 10) {
-        direction = 'right';
-        img.style.transform = 'scaleX(1)';
-      }
-  } else if (option === 3) {
-    if (direction === 'right' && Math.abs((viewport/2) - pos)  < 10) {
-      pos += 20;
-      img.src = 'giphy.gif'; //'https://media.giphy.com/media/4BJCvMoLPePq8/giphy.gif';
-      clearInterval(intervalId);
-      setTimeout(function() {
-        intervalId = setInterval(function() {
-          img.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
-          catWalk(3);
-        }, 50);
-      }, 2000);
-    } else if (direction === 'right' && pos < viewport) {
-      pos += 10;
-    } else if (direction === 'right' && pos >= viewport) {
-      direction = 'left';
-      img.style.transform = 'scaleX(-1)';
-    } else if (direction === 'left' && Math.abs(pos - (viewport/2)) < 10 ) {
-      clearInterval(intervalId);
-      img.src = 'giphy.gif'; //'https://media.giphy.com/media/4BJCvMoLPePq8/giphy.gif';
-      pos -= 20;
-      setTimeout(function() {
-        intervalId = setInterval(function() {
-          img.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
-          catWalk(3);
-        }, 50);
-      }, 2000);
-    } else if (direction === 'left' && pos > viewport + 10) {
-      pos -= 10;
-    } else if (direction === 'left' && pos >= 10) {
-      pos -= 10;
-    } else {
-      direction = 'right';
-      img.style.transform = 'scaleX(1)';
-    }
-  } else {
-    crazy();
+  switch(option) {
+    case 1:
+            walkTask1();
+            break;
+    case 2:
+            walkTask2();
+            break;
+    case 3:
+            walkTask3();
+            break;
+    default:
+            crazy();
   }
-  img.style.left = pos + 'px';
-
 };
-
 
 
 intervalId = setInterval(function() {
-  catWalk(3);
+  //catWalk(1);
+  //catWalk(2);
+  //catWalk(3);
+  crazy();
 }, 50);
