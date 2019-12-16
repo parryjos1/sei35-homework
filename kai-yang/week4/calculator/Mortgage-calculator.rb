522e796b2b12e0f42b23e961ea3c94d3a9fba391
Kai Yang <kaiyang.ky2@gmail.com>
2019-12-16 22:57:21 +1100

    week4 homework mortgage calculator

--- kai-yang/week4/calculator/Mortgage-calculator.rb
+++ kai-yang/week4/calculator/Mortgage-calculator.rb
@@ -1,3 +1,12 @@
-
 # Mortgage Calculator
 # Calculate the monthly required payment given the other variables as input (look up the necessary variables)
+def mortgage
+  print "Please type your loan($): "
+  loan = gets.to_f
+  print "Please type your term of months: "
+  n = gets.to_f
+  print "Please type your monthly interest rate: "
+  rate = gets.to_f
+  loan * (rate * (1 + rate) ** n)/((1 + rate) ** n - 1)
+end
+ puts "The monthly required payment is #{mortgage}"
