30400be0a41e954a43de386b49671c09175aaff9
Josh Parry <josh.parry97@gmail.com>
2020-01-06 10:11:40 +1100

    wk5d1 warmup solution

---
+++ warmups/week5/day_01_simple_sums/simple-sums.rb
@@ -0,0 +1,42 @@
+
+class SimpleSums
+
+    def initialize( number )
+        @number = number
+    end
+
+    def number
+        @number
+    end
+
+    def s1
+        @number % 2
+    end 
+
+    def s2
+
+        # Option 1
+        # sum_s2 = (@number * (@number + 1)) / 2
+        # sum_s2
+        sum_s2 = 0
+
+        # (1..@number).each do |num| 
+        #     sum_s2 += num
+        # end 
+
+        @number.times do |num| 
+            sum_s2 += num + 1
+        end 
+        sum_s2
+
+        # Option 3
+        # (1..@number).sum
+    end 
+
+end
+
+sum = SimpleSums.new(4)
+
+p sum.number 
+p sum.s1
+p sum.s2 
