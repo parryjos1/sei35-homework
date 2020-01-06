6240430aa69f1a8f3cf5dab346097c63b1dfe1bd
Lawrence Carreon <l.carreon123@gmail.com>
2020-01-07 07:26:46 +1100

    completed rails games

--- lawrence-carreon/week5/games-on-rails/app/controllers/magicconch_controller.rb
+++ lawrence-carreon/week5/games-on-rails/app/controllers/magicconch_controller.rb
@@ -2,5 +2,17 @@ class MagicconchController < ApplicationController
     def home
     end
     def user_response
+        @user_question = params[:question].gsub("-"," ")
+        responses = [
+            "Maybe someday.",
+            "Nothing.",
+            "Neither.",
+            "Follow the seahorse.",
+            "I don't think so.",
+            "No.",
+            "Yes.",
+            "Try asking again."
+        ]
+        @magic_response = responses[rand(0...7)] 
     end
 end
--- lawrence-carreon/week5/games-on-rails/app/controllers/secretnumber_controller.rb
+++ lawrence-carreon/week5/games-on-rails/app/controllers/secretnumber_controller.rb
@@ -2,5 +2,8 @@ class SecretnumberController < ApplicationController
     def home
     end
     def played
+        @user_guess = params[:guess].to_i
+        @magic_number = rand(1...10)
+        @correct = @user_guess == @magic_number ? "You guessed... Wisely!" : "You guessed... poorly..."
     end
 end
