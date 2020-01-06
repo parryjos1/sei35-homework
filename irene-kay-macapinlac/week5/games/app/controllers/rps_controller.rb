43e3b186d437ac6efcd7601158753ccf5f3aa2e2
irene macapinlac <irenekaymac@gmail.com>
2020-01-07 09:31:54 +1100

    finished rps

--- irene-kay-macapinlac/week5/games/app/controllers/rps_controller.rb
+++ irene-kay-macapinlac/week5/games/app/controllers/rps_controller.rb
@@ -8,17 +8,29 @@ class RpsController < ApplicationController
       "scissors"
     ]
 
-    game_logic = [
-      "rock" > "scissors",
-      "scissors" > "paper",
-      "paper" > "rock"
-    ]
+    game_logic = {
+      "rock" => "scissors",
+      "scissors" => "paper",
+      "paper" => "rock"
+    }
 
     # Use params[:throw] as a user's choice
     @p1_hand = params[:hand]
     @ai_hand = game_hands.sample #need to code a way that this hand can't choose p1's hand
 
-    @result = 
+    @result = nil
+
+    if game_logic[@p1_hand] == @ai_hand
+      @result = 'User wins'
+    elsif @p1_hand == @ai_hand
+      @result = 'Draw'
+    else
+      @result = 'AI wins'
+
+    
+
+
+
 
 
 
