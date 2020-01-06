2eec0c5d10aef1160ae1447f8b0ee1fb07348002
Kai Yang <kaiyang.ky2@gmail.com>
2020-01-06 23:19:37 +1100

    week5 homework rock paper scissors

--- kai-yang/week5/games-on-rails/config/routes.rb
+++ kai-yang/week5/games-on-rails/config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # choose games
+  get '/choose_games' => 'cg#home'
+
+
   # Magic 8 Ball
   # 1. get user's questions
-  get '/questions' => 'magic8ball#ask_questions'
+  get '/choose_games/questions' => 'magic8ball#ask_questions'
   # 2. answer questions
-  get '/questions/answers' => 'magic8ball#answer_questions'
+  get '/choose_games/questions/answers' => 'magic8ball#answer_questions'
 
   # Secret Number
   # 1. submit numbers
-  get '/numbers' => 'secretnumber#pick_number'
+  get '/choose_games/numbers' => 'secretnumber#pick_number'
   # 2. show result
-  get '/numbers/result' => 'secretnumber#show_result'
+  get '/choose_games/numbers/result' => 'secretnumber#show_result'
+
+  # Rock Paper Scissors
+  # 1.Choose
+  get '/choose_games/rock_paper_scissors' => 'rps#play'
+  # 2. check
+  get '/choose_games/rock_paper_scissors/:throw' => 'rps#check'
 end
