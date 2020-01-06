8c93de77ea586140f34c9b4bcc8d21b198c4e597
irene macapinlac <irenekaymac@gmail.com>
2020-01-07 00:32:58 +1100

    got secretnumber to work using a conditional statement

--- irene-kay-macapinlac/week5/games/app/controllers/secretnum_controller.rb
+++ irene-kay-macapinlac/week5/games/app/controllers/secretnum_controller.rb
@@ -9,19 +9,22 @@ class SecretnumController < ApplicationController
     # can also write as rand 1..10
 
     @random_num = Random.rand(1..10)
-    @user_num = params[:guessed_num]
+    @user_num = params[:guessed_num].to_i
 
+    result_true = "YOU WIN!"
 
-    if (@user_num == @random_num)
-      puts "YOU WIN!"
-    else
-      puts "Awwwww, try again!"
-    end
+    result_false = "Awwwww, try again!"
 
+      # if (@user_num == @random_num)
+      #   puts "YOU WIN!"
+      # else
+      #   puts "Awwwww, try again!"
+      # end
 
-    binding.pry
+      @result = @user_num == @random_num ? result_true : result_false
 
-    # @results = response['results']
+    # binding.pry
+
 
   end
 
