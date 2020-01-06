efae7eaabfcfaf6387ce8be4b1c7a3c9f42b6ed8
irene macapinlac <irenekaymac@gmail.com>
2020-01-06 23:37:56 +1100

    finished magic 8 ball game

---
+++ irene-kay-macapinlac/week5/games/config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Magic 8 Ball
+
+  # type the question on the URL and the mball#answer_to will generate a random answer
+  get '/mball:query' => 'mball#answer_to'
+  # mball#ask
+
+  # Secret Number
+
+  # 1. Blank form
+  get '/secretnum' => 'secretnum#form'
+
+  # 2. Form submits here, does stock lookup, prints results
+  get '/secretnum/lookup' => 'secretnum#do_lookup'
+
+  # Rock Paper Scissors
+
+  # 1. Blank form
+  get '/rps' => 'rps#form'
+
+  # 2. Form submits here, does stock lookup, prints results
+  get '/rps/lookup' => 'rps#do_lookup'
+
+end
