e55cf40901e22a850b079038eee034c156536a37
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-02-14 01:21:43 +1100

    post request not working

---
+++ gabriel-hilti/week10/burning-airlines-express-mongodb/seeds.js
@@ -0,0 +1,38 @@
+[
+  {
+    "id": 14,
+    "flight_number": "123",
+    "origin": "SYD",
+    "destination": "SFO",
+    "departure_date_formatted": ISODate("2018-12-25T20:20:00Z"),
+    "airplane": { "name": "757", "rows": 50, "cols": 8 },
+    "reservations": [
+      { "row":10, "col": 3 },
+      { "row":11, "col": 2 }
+    ]
+  },
+  {
+    "id": 13,
+    "flight_number": "456",
+    "origin": "SYD",
+    "destination": "SFO",
+    "departure_date_formatted": ISODate("2018-12-30T12:12:00Z"),
+    "airplane": { "name": "737", "rows": 40, "cols": 4 },
+    "reservations": [
+      { "row":1, "col": 2, "passenger": { "id": 13, "name": "test guy", "email": "test@guy.com"} },
+      { "row":2, "col": 3, "passenger": { "id": 14, "name": "other guy", "email": "other@guy.com"} }
+    ]
+  },
+  {
+    "id": 15,
+    "flight_number": "789",
+    "origin": "SYD",
+    "destination": "SIN",
+    "departure_date_formatted": ISODate("2018-12-31T23:23:23Z"),
+    "airplane": { "name": "757", "rows": 50, "cols": 8 },
+    "reservations": [
+      { "row":5, "col": 6 },
+      { "row":5, "col": 7 }
+    ]
+  }
+]
