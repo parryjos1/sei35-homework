f50199246fa77a0c4999d7f6b02bbb79f2c3e19f
Ana Silva <apgsilva81@gmail.com>
2020-02-17 10:21:57 +1100

    w11 d1 warmup solution

--- warmups/week11/day01_pairwise/pairwise.js
+++ warmups/week11/day01_pairwise/pairwise.js
@@ -10,8 +10,8 @@ const pairwise = (arr, sum) => {
 
       if(arr[i] + arr[j] === sum) {
         result += i + j; //add indexes of values found
-        console.log('i', i);
-        console.log('j', j);
+        // console.log('i', i);
+        // console.log('j', j);
         arr[i] = '';//so it's not reused
         arr[j] = '';//so it's not reused
 
@@ -19,12 +19,12 @@ const pairwise = (arr, sum) => {
     }//loop j
   } //loop i
 
-  return result;
+  return result; //the final addition of all indexes
 }
 
-// console.log(pairwise([7,9,11,13,15], 20)); //6
-// console.log(pairwise([7,5,11,5,15], 10)); //4
-// console.log(pairwise([1,4,2,3,0,5], 7)); //11
+console.log(pairwise([7,9,11,13,15], 20)); //6
+console.log(pairwise([7,5,11,5,15], 10)); //4
+console.log(pairwise([1,4,2,3,0,5], 7)); //11
 
 
 
@@ -33,21 +33,22 @@ const pairwise = (arr, sum) => {
 const pairwise2 = (arr, sum) => {
 
   const result = arr.reduce( (acc, cur, index) => {
-
+    //calculate the complement number to cur
     let theOtherElement = sum - cur;
 
+    //if we find complement number and it's not the same as cur
     if (arr.indexOf(theOtherElement) !== -1 && arr.indexOf(theOtherElement) !== index){
-      console.log('index', index);
-      console.log('index complement', arr.indexOf(theOtherElement));
+      // console.log('index', index);
+      // console.log('index complement', arr.indexOf(theOtherElement));
 
-      let total = index + arr.indexOf(theOtherElement);
+      let total = index + arr.indexOf(theOtherElement);//sum their indexes
 
-      arr.splice(index, 1, NaN);
-      arr.splice(arr.indexOf(theOtherElement), 1, NaN);
+      arr.splice(index, 1, NaN); //so we don't reuse it
+      arr.splice(arr.indexOf(theOtherElement), 1, NaN); //so we don't reuse it
 
       return acc + total;  //update the acc
     }
-    console.log('acc', acc);
+    // console.log('acc', acc);
     return acc;
   }, 0); //initial acc value is 0
 
